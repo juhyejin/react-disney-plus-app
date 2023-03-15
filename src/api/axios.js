3f3a03b9c048a8e65a3b5d8c4c8bf6b8cd0f9e85
juhyejin <juhyejin.dev@gmail.com>
2023-03-15 17:24:25 +0900

    :closed_lock_with_key: axios instance

---
+++ src/api/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key : process.env.REACT_APP_MOVIE_DB_API,
+    language: "ko-KR"
+  }
+})
+export default instance;
