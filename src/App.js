4d47132be0a72efece2f9d221f32f8885b236e74
juhyejin <juhyejin.dev@gmail.com>
2023-03-15 21:06:24 +0900

    :sparkles: 배너 제작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
+import styled from 'styled-components'
+import Banner from "./components/Banner";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Container>
+        <Nav />
+        <Banner/>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc( 100vh - 250px);
+  overflow: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc( 3.5vw + 5px);
+  
+  &::after{
+    content: "";
+    background-image: url("/images/home-background.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+  }
+`
