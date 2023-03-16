caa4d4c57e7b16cc6e58ad278d1f6850c7dff299
juhyejin <juhyejin.dev@gmail.com>
2023-03-16 21:39:36 +0900

    :sparkles: 슬라이드 기능 추가-> 추후 ref속성으로 변경

--- src/components/Row.js
+++ src/components/Row.js
@@ -15,15 +15,22 @@ const Row = ({title, id, fetchUrl}) => {
     fetchMovieData();
   }, [fetchMovieData]);
 
+  const clickArrowSlideMovie = (direction) => {
+    if(direction === "left"){
+      document.getElementById(id).scrollLeft -= window.innerWidth - 80 ;
+    }else{
+      document.getElementById(id).scrollLeft += window.innerWidth - 80 ;
+    }
+  }
 
   return (
     <div>
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span className="arrow" onClick={()=>clickArrowSlideMovie("left")}>{"<"}</span>
         </div>
-        <div id="id" className="row__posters">
+        <div id={id} className="row__posters">
           {movies.map(movie => (
             <img
               key={movie.id}
@@ -34,7 +41,7 @@ const Row = ({title, id, fetchUrl}) => {
           ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{">"}</span>
+          <span className="arrow" onClick={()=>clickArrowSlideMovie()}>{">"}</span>
         </div>
       </div>
     </div>
